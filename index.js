ce1307c33f3c41bdc866027c040941072a1ab33d
ariful-islam <arifulislam204909@gmail.com>
2023-05-19 19:10:17 +0600

    basic server set up done

--- index.js
+++ index.js
@@ -8,15 +8,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-
-//arifulislam204909
-
-//gWTHk003tGELidsM
-
 app.use(cors());
 app.use(express.json());
 
-
+//mongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g0kwdaj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,12 +32,14 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
 
 
+
+//testing server
 app.get("/", (req, res) => {
   res.send("BABY CAR is running");
 });
